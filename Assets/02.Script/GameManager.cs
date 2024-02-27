b25195f384ae5c2d651cb50f960debc9b1d900ea
yen-world <dlddukin@gmail.com>
2024-02-27 20:28:52 +0900

    feat: GameManager 싱글톤화 구현

--- Assets/02.Script/GameManager.cs
+++ Assets/02.Script/GameManager.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
