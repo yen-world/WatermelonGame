1b52c8fc6137f42d0ff838755957f3d01268320a
yen-world <dlddukin@gmail.com>
2024-03-27 16:20:17 +0900

    fix: 최종 과일끼리 충돌 시 진화를 수행하지 않도록 수정

--- Assets/02.Script/FruitManager.cs
+++ Assets/02.Script/FruitManager.cs
@@ -37,36 +37,12 @@ public class FruitManager : MonoBehaviour
 
     public void EvolutionFruit(Vector2 collisionPoint, int level)
     {
-        GameObject fruit = Instantiate(GameManager.Instance.Fruits[level], collisionPoint, Quaternion.identity);
-        fruit.GetComponent<Rigidbody2D>().isKinematic = false;
-        fruit.GetComponent<Fruit>().isDropped = true;
-
-        // switch (level)
-        // {
-        //     case 1:
-        //     Instantiate
-        //         break;
-        //     case 2:
-        //         break;
-        //     case 3:
-        //         break;
-        //     case 4:
-        //         break;
-        //     case 5:
-        //         break;
-        //     case 6:
-        //         break;
-        //     case 7:
-        //         break;
-        //     case 8:
-        //         break;
-        //     case 9:
-        //         break;
-        //     case 10:
-        //         break;
-        //     case 11:
-        //         break;
-        // }
+        if (level != 11)
+        {
+            GameObject fruit = Instantiate(GameManager.Instance.Fruits[level], collisionPoint, Quaternion.identity);
+            fruit.GetComponent<Rigidbody2D>().isKinematic = false;
+            fruit.GetComponent<Fruit>().isDropped = true;
+        }
     }
 
     public void GetNextFruit()
