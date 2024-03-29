cbd8f6918288c434109d068d2a4480546b3b0418
yen-world <dlddukin@gmail.com>
2024-03-29 16:56:28 +0900

    fix: 메뉴 UI가 떠있을 경우 구름을 컨트롤 못하게 수정

--- Assets/02.Script/CloudController.cs
+++ Assets/02.Script/CloudController.cs
@@ -7,35 +7,40 @@ public class CloudController : MonoBehaviour
     Vector2 vector;
     [SerializeField] float speed;
     FruitManager theFruit;
+    MainButton mainButtonScript;
 
     // Start is called before the first frame update
     void Start()
     {
         theFruit = FindObjectOfType<FruitManager>();
+        mainButtonScript = FindObjectOfType<MainButton>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (!mainButtonScript.isActiveMenu)
         {
-            if (this.transform.position.x > -1.75f)
+            if (Input.GetKey(KeyCode.LeftArrow))
             {
-                vector.Set(-1, 0);
-                this.transform.Translate(vector * speed * Time.deltaTime);
+                if (this.transform.position.x > -1.75f)
+                {
+                    vector.Set(-1, 0);
+                    this.transform.Translate(vector * speed * Time.deltaTime);
+                }
             }
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            if (this.transform.position.x < 3.3f)
+            if (Input.GetKey(KeyCode.RightArrow))
             {
-                vector.Set(1, 0);
-                this.transform.Translate(vector * speed * Time.deltaTime);
+                if (this.transform.position.x < 3.3f)
+                {
+                    vector.Set(1, 0);
+                    this.transform.Translate(vector * speed * Time.deltaTime);
+                }
             }
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            theFruit.DropFruit();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                theFruit.DropFruit();
+            }
         }
     }
 }
